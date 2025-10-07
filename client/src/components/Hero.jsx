06b8a3759a78633d0f169d86aa642f9114d3f630
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-10-07 23:19:16 +0530

    Update hero section text content

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage6.jpg")] bg-no-repeat bg-cover bg-center h-screen relative before:content-[""] before:absolute before:inset-0 before:bg-black/40'> 
       <div className='relative z-10'>
-        <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-4xl w-fit mt-20'>The Ultimate Hotel Experience</p>
-        <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Discover Your Perfect Gateway Destination</h1>
-        <p className='max-w-130 mt-2 text-sm md:text-base'>Unparalleled luxury and comfort awaits at the world's most exclusive hotels and resorts. Start your journey today.</p>
+        <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-4xl w-fit mt-20'>Your Perfect Stay, One Click Closer.</p>
+        <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Stay, Relax, and Reimagine Luxury</h1>
+        <p className='max-w-130 mt-2 text-sm md:text-base'>Indulge in world-class comfort and hospitality wherever your journey takes you.</p>
         
         <form className='bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
           <div>
