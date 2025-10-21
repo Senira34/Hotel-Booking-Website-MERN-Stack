8ad9e3d7add23b171856f8b2952837d54a321a33
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-10-22 01:50:08 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRotes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 connectDB()
+connectCloudinary();
 
 const app = express()
 app.use(cors())
@@ -18,6 +22,8 @@ app.use(clerkMiddleware())
 app.use('/api/clerk' , clerkWebhooks);
 
 app.get('/', (req, res) => res.send("API is working"))
+app.use('/api/user', userRouter);
+app.use('/api/hotel', hotelRouter);
 
 const PORT = process.env.PORT || 3000;
 
